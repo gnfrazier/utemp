0e0d3947889b34622defb355e37d01a3e23910fc
Greg <gnfrazier@gmail.com>
2017-01-18 06:22:29 -0500

    Added rcv function block.

--- msg.py
+++ msg.py
@@ -1,15 +1,40 @@
 from umqtt.simple import MQTTClient
 
 
-def message(**kwargs, server="localhost", topic="MQTT", subject="empty message"):
+def xmt(server="localhost", topic="MQTT", note="empty message"):
     c = MQTTClient("umqtt_client", server)
     c.connect()
-    c.publish(bytes(topic, 'UTF-8'), bytes(subject, 'UTF-8'))
+    c.publish(bytes(topic, 'UTF-8'), bytes(note, 'UTF-8'))
+    c.disconnect()
+
+
+def sub_cb(topic, msg):
+    print((topic, msg))
+
+
+def rcv(server="localhost", topic="MQTT"):
+
+    c = MQTTClient("umqtt_client", server)
+    c.set_callback(sub_cb)
+    c.connect()
+    c.subscribe(bytes(topic, 'UTF-8'))
+    while True:
+        if True:
+            # Blocking wait for message
+            c.wait_msg()
+        else:
+            # Non-blocking wait for message
+            c.check_msg()
+            # Need to sleep to avoid 100% CPU usage (in a real
+            # app other useful actions would be performed instead)
+            time.sleep(1)
+
     c.disconnect()
 
 
 def main():
-    message()
+    xmt()
+    rcv()
 
 if __name__ == "__main__":
     main()
